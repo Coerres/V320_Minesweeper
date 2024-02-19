e3d1913801f9492fa73c67d262fddfb20b5171e0
Carina Cordes <carina.cordes@ksb-sg.ch>
2024-02-19 12:19:12 +0100

    Change 'Modul.cs'

--- Minesweeper.Logic/Model.cs
+++ Minesweeper.Logic/Model.cs
@@ -1,14 +1,28 @@
 ﻿namespace Minesweeper.Logic
+   
 {
     public class Model
     {
         private Field[,] gameBoard = new Field[20, 20];
-
+        
         public void DoTurn(string coordinate)
         {
+
+            foreach (var field in gameBoard)
+            {
+                Console.WriteLine("Fortnite");
+            }
+
+            Console.WriteLine("Which field do you want to change?");
+            string input = Console.ReadLine();
+            input.ToUpper();
+
 
         }
     }
 
-    public class Field { }
+    public class Field 
+    {
+
+    }
 }
